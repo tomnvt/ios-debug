c363bb7bdb7e19cea18af67e83394b3c5ba2bd3e
tomnvt <tom.novota@gmail.com>
2021-04-23 22:34:08 +0200

    Remove old run scipt file

--- .ios-debug_sync.py
+++
@@ -1,8 +0,0 @@
-from iosdebug.stop import stop
-from iosdebug.start import start
-
-if os.environ["ENABLE_PREVIEWS"] == "YES":
-    raise SystemExit
-
-stop(update_project=False)
-start(update_project=False)
