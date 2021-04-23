cf9985f896f6fd86a878a7230923e1053458f0de
Tomáš Novotný <tomas.novotny@synetech.cz>
2021-04-23 21:48:46 +0200

    VERSION 0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ios-debug",
-    version="0.0.0.36",
+    version="0.1.0",
     description="Debug mode manager for iOS.",
     long_description=README,
     long_description_content_type="text/markdown",
